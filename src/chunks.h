81e4c1b228aa357a41bcd2cfdefefd496a6b4074
imma-Spring <jairjair511@gmail.com>
2023-11-20 09:41:09 -0500

    added [chunks]

---
+++ src/chunks.h
@@ -0,0 +1,15 @@
+#include "common.h"
+
+typedef enum Op_Code {
+  OP_RETURN,
+} Op_Code;
+
+typedef struct Chunk {
+  size_t count;
+  size_t capacity;
+  uint8_t *code;
+} Chunk;
+
+void init_chunk(Chunk *chunk);
+void write_chunk(Chunk *chunk, uint8_t byte);
+void free_chunk(Chunk *chunk);
